ea5a2788be17fb715165b015675e7d6cd763ed5a
CanFlyHPPC <314373866@qq.com>
2022-04-23 18:06:46 +0800

    走100步法

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
+{
+	for (float t = 0; t < 1.0; t += 0.01)//直线参数t渐进，走100步
+	{
+		int x = x0 + (x1 - x0) * t;//int可以每个像素归整
+		int y = y0 + (y1 - y0) * t;
+		image.set(x, y, color);
+	}
+}
+
 int main(int argc, char** argv)
 {
 	TGAImage image(100, 100, TGAImage::RGB); //纯黑的100 * 100图
-
+	line(13, 20, 80, 40, image, white);
 	image.set(52, 41, red);//在52，41位置画一个红点，DX坐标系，原点在左上角
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
