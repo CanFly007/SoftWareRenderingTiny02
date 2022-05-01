7c327215d9659eac3548734840f8259ca40f94fa
MrHe <314373866@qq.com>
2022-05-01 15:36:50 +0800

    更换重心坐标算法，结果正确

--- main.cpp
+++ main.cpp
@@ -59,30 +59,51 @@ Vec3f barycentric(Vec3f* trianglePtr, Vec3f P)
 	Vec3f B = trianglePtr[1];
 	Vec3f C = trianglePtr[2];
 
-	Vec3f crossABC = (B - A) ^ (C - A); //ABC ABP APC PBC都是按点P在三角形内顺时针顺序组合的
-	Vec3f crossABP = (B - A) ^ (P - A);
-	Vec3f crossAPC = (P - A) ^ (C - A);
-	Vec3f crossPBC = (B - P) ^ (C - P);
-
-	//叉积的方向，如果点P在三角形外，叉积结果就会有正负之分
-	//新学到方法，用点积判断方向,同侧点积为正
-	float outsideAB = crossABC * crossABP;//如果点积为负数，表示ABP是逆时针的，P在AB边外侧
-	float outsideAC = crossABC * crossAPC;
-	float outsideBC = crossABC * crossPBC;
-	if (outsideAB < 0 || outsideAC < 0 || outsideBC < 0)
-		return Vec3f(-1, -1, -1);//满足其中一个，则不在三角形内，不用下面计算了，也可以在下面uvw时候加负号,AB边外则对着w权重为负数
-
-	//叉积的模表示两个向量的所围成的平行四边形面积，用来计算重心坐标
-	float areaABC = crossABC.norm() * 0.5; //如果点P在三角形外，叉积结果就会有正负之分
-	float areaABP = crossABP.norm() * 0.5;
-	float areaAPC = crossAPC.norm() * 0.5;
-	float areaPBC = crossPBC.norm() * 0.5;
+	Vec3f s[2];
+	s[0] = Vec3f(C.x - A.x, B.x - A.x, A.x - P.x);
+	s[1] = Vec3f(C.y - A.y, B.y - A.y, A.y - P.y);
+	//for (int i = 2; i--; ) 
+	//{
+	//    s[i].x = C[i] - A[i];
+	//    s[i].y = B[i] - A[i];
+	//    s[i].z = A[i] - P[i];
+	//}
+	Vec3f u = s[0] ^ s[1];
+	if (std::abs(u.z) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);//因为u的结果是(u,v,1)
+	return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 
 
-	float u = areaPBC / areaABC;
-	float v = areaAPC / areaABC;
-	float w = areaABP / areaABC;
-	return Vec3f(u, v, w);
+
+
+	//Vec3f A = trianglePtr[0];
+	//Vec3f B = trianglePtr[1];
+	//Vec3f C = trianglePtr[2];
+
+	//Vec3f crossABC = (B - A) ^ (C - A); //ABC ABP APC PBC都是按点P在三角形内顺时针顺序组合的
+	//Vec3f crossABP = (B - A) ^ (P - A);
+	//Vec3f crossAPC = (P - A) ^ (C - A);
+	//Vec3f crossPBC = (B - P) ^ (C - P);
+
+	////叉积的方向，如果点P在三角形外，叉积结果就会有正负之分
+	////新学到方法，用点积判断方向,同侧点积为正
+	//float outsideAB = crossABC * crossABP;//如果点积为负数，表示ABP是逆时针的，P在AB边外侧
+	//float outsideAC = crossABC * crossAPC;
+	//float outsideBC = crossABC * crossPBC;
+	//if (outsideAB < 0 || outsideAC < 0 || outsideBC < 0)
+	//	return Vec3f(-1, -1, -1);//满足其中一个，则不在三角形内，不用下面计算了，也可以在下面uvw时候加负号,AB边外则对着w权重为负数
+
+	////叉积的模表示两个向量的所围成的平行四边形面积，用来计算重心坐标
+	//float areaABC = crossABC.norm() * 0.5; //如果点P在三角形外，叉积结果就会有正负之分
+	//float areaABP = crossABP.norm() * 0.5;
+	//float areaAPC = crossAPC.norm() * 0.5;
+	//float areaPBC = crossPBC.norm() * 0.5;
+
+
+	//float u = areaPBC / areaABC;
+	//float v = areaAPC / areaABC;
+	//float w = areaABP / areaABC;
+	//return Vec3f(u, v, w);
 }
 
 //传入的是屏幕空间坐标,构建三角形(trianglePtr)的包围盒。逐一判断包围盒里面每个像素，是否在三角形内（重心坐标判断法）
